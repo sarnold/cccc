ed2a809f27e6a0264ad03af909ddf30cede5397e
tim_littlefair <tim_littlefair@5922d1d1-9610-0410-a011-a6bce15726ed>
2002-04-18 16:08:13 +0000

    Fixes for Win32 installer environment

--- ccccdist/w32installer/ccccwrap.bat
+++ ccccdist/w32installer/ccccwrap.bat
@@ -1,7 +1,8 @@
 @echo off
 doskey > NUL
 type ccccwrap.msg
-rem The remainder of this file will be created by the action of 
-rem the postinstallation script make_ccccwrap.bat
+call cccc_env.bat
+path %CCCC_PATH_DIR%
+cd %CCCC_WORK_DIR%
 
 
