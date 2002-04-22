abaa2cddce05eeb71c6060ac346c2a6c865a172e
tim_littlefair <tim_littlefair@5922d1d1-9610-0410-a011-a6bce15726ed>
2002-04-22 14:48:03 +0000

    These modifications create an initial working command line environment for CCCC.

--- ccccdist/w32installer/make_cccc_env.bat
+++ ccccdist/w32installer/make_cccc_env.bat
@@ -5,7 +5,5 @@ rem the file cccc_env.bat which is included by ccccwrap.bat
 rem to define the directory where the CCCC binary is and
 rem the working directory in which the command prompt is 
 rem to start.
-echo set CCCC_PATH_DIR=%1 >> cccc_env.bat
-echo set CCCC_WORK_DIR=%2 >> cccc_env.bat
-
-
+echo set CCCC_PATH_DIR=%1> cccc_env.bat
+echo set CCCC_WORK_DIR=%2>> cccc_env.bat
