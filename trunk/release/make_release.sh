395154e3107ec1acbcf1f9497575286d3ae4e92b
tim_littlefair <tim_littlefair@5922d1d1-9610-0410-a011-a6bce15726ed>
2002-04-23 16:02:09 +0000

    Removed -z9 on export as it appears to cause a hang.

--- trunk/release/make_release.sh
+++ trunk/release/make_release.sh
@@ -13,8 +13,7 @@ release_name=cccc-$1.$2
 release_number=$1.$2
 
 cvs rtag $release_tag .
-cvs -z9 export -r $release_tag -d $release_name ccccdist
-echo "#define CCCC_VERSION $release_number" > $release_name/cccc/cccc_ver.h
+cvs export -r $release_tag -d $release_name ccccdist
+echo "#define CCCC_VERSION \"$release_number\"" > $release_name/cccc/cccc_ver.h
 tar czvf $release_name.tar.gz $release_name
-su -c cp $release_name.tar.gz /mnt/win_c
 
