b57f4411cd2672fec2d73d8ece423bd5b5118a5f
tim_littlefair <tim_littlefair@5922d1d1-9610-0410-a011-a6bce15726ed>
2002-05-01 14:23:44 +0000

    Modified to make the installer work.

--- ccccdist/w32installer/ccccwrap.bat
+++ ccccdist/w32installer/ccccwrap.bat
@@ -2,7 +2,7 @@
 set PATH=%WINDIR%;%WINDIR%\command;%WINDIR%\system;%WINDIR%\system32
 type ccccwrap.msg
 call cccc_env.bat
-doskey /echo:off cccc=%CCCC_PATH_DIR%\CCCC.exe
+doskey /echo:off cccc=%CCCC_PATH_DIR%\CCCC.exe $* $T start .cccc\cccc.html
 cd c:\
 %COMSPEC% 
 
