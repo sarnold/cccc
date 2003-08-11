969e8f72f091762e8f0a6d083ef177438109a709
tim_littlefair <tim_littlefair@5922d1d1-9610-0410-a011-a6bce15726ed>
2003-08-11 05:15:25 +0000

    Reworked command line interface.

--- ccccdist/w32installer/ccccwrap.bat
+++ ccccdist/w32installer/ccccwrap.bat
@@ -1,8 +1,44 @@
 @echo off 
-set PATH=%WINDIR%;%WINDIR%\command;%WINDIR%\system;%WINDIR%\system32 
-set CCCC_PATH_DIR="C:\Program Files\CCCC"
-set CCCC_WORK_DIR="C:\"
-type %CCCC_PATH_DIR%\ccccwrap.msg 
-doskey cccc=%CCCC_PATH_DIR%\CCCC.exe $* $T start .cccc\cccc.html 
+set CCCC_ENV=cccc_env.bat
+set CCCC_EXE=cccc.exe
+
+if not exist %CCCC_ENV% goto noEnv
+call %CCCC_ENV%
+
+if not exist %CCCC_PATH_DIR%\%CCCC_EXE% goto noExe
+if not exist %CCCC_WORK_DIR% goto noWorkDir
+
+path %CCCC_PATH_DIR%;%path%
 cd %CCCC_WORK_DIR% 
+
+rem Request the usage message.
+%CCCC_PATH_DIR%\cccc.exe --help
+
+rem Start the shell
 %COMSPEC% 
+goto end
+
+:noEnv
+echo The file %CCCC_ENV% was not found.
+echo This file is required to define the command line environment for CCCC.
+echo Please uninstall and reinstall CCCC to get a working command line environment.
+pause
+goto end
+
+:noExe
+echo The executable %CCCC_EXE% was not found in %CCCC_PATH_DIR%.
+echo Please uninstall and reinstall CCCC to get a working command line environment.
+pause
+goto end
+
+:noWorkDir
+echo The shell variable CCCC_WORK_DIR is defined as %CCCC_WORK_DIR% but this 
+echo directory does not exist.
+echo This variable is required to define the initial work directory
+echo for a session using the CCCC command line environment.
+echo Please uninstall and reinstall CCCC to get a working command line environment.
+pause
+goto end
+
+:end
+
